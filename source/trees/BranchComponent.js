e3f8fa21f705b03824ca989ddd825b8f14f379b5
huediemdue <huediemdue@gmail.com>
2019-05-10 10:55:42 +0700

    Create tree with animation

--- source/trees/BranchComponent.js
+++ source/trees/BranchComponent.js
@@ -1,188 +1,118 @@
-
 import React from 'react';
-import { View, StyleSheet, Animated, Dimensions } from 'react-native';
+import {
+  View,
+  StyleSheet,
+  Animated,
+  Dimensions,
+  ScrollView,
+  Text,
+} from 'react-native';
 import Svg, { Path } from 'react-native-svg';
-import ImageAvatar from './ImageAvatar';
-import Images from '../assets/images'
-import _ from 'lodash'
+import { getLine } from '../utils/HelperLines';
 let path = require('svg-path-properties');
 
 const { width, height } = Dimensions.get('window');
 
+const data = [
+  {
+    point1: { x: 350, y: 400 },
+    point2: { x: 50, y: 300 },
+    options: {
+      type: 'Left',
+    },
+  },
+  {
+    point1: { x: 50, y: 300 },
+    point2: { x: 300, y: 100 },
+    options: {
+      type: 'BoundedRight',
+    },
+  },
+  {
+    point1: { x: 50, y: 300 },
+    point2: { x: 50, y: 100 },
+    options: {
+      type: 'BoundedLeft',
+      distance: 125,
+    },
+  },
+];
+
 export default class BranchComponent extends React.Component {
   constructor(props) {
     super(props);
-    const { options, data } = this.props
-    const point1 = data[0]
-    const point2 = data[1]
-    const point3 = data[2]
-    this.d1 = this.getLine(point1, point2, options)
-    this.d2 = this.getLine(point1, point3, options)
-    this.properties1 = path.svgPathProperties(this.d1);
-    this.properties2 = path.svgPathProperties(this.d2);
-    this.tl1 = this.properties1.getTotalLength();
-    this.tl2 = this.properties2.getTotalLength();
-    this.animateValue1 = new Animated.Value(900);
-    this.animateValue2 = new Animated.Value(900);
+    this.state = {
+      data: data,
+    };
+    this.animatedValue = [];
+    data.map(() => {
+      this.animatedValue.push(new Animated.Value(1000));
+    });
   }
 
   componentDidMount() {
-    // this.animate();
+    this.createAllTree();
   }
 
-  shouldComponentUpdate(nextProps, nextState) {
-    const nextData = _.get(nextProps, 'data')
-    const currData = _.get(this.props, 'data')
-    console.log('shouldComponentUpdate ', nextProps)
-
-    if (nextProps.isAnimated && !_.isEqual(nextData, currData)) {
-      console.log('shouldComponentUpdate ', nextData)
-      return false
-      // this.animate()
-    }
-    return true
-  }
-
-  rightRoundedRect = (point1, point2, options) => {
-    // options.radius = 40;
-    return (
-      'M' + point1.x + ',' + point1.y +
-      'L' + point1.x + ' ' + (point2.y + options.radius) +
-      'a' + options.radius + ',' + options.radius + ' 0 0 1 ' + options.radius + ',' + -options.radius +
-      'L' + point2.x + ' ' + point2.y
-    );
-  };
-
-  leftLineRect = (point1, point2, options) => {
-    // const radius = 40
-    return (
-      'M' + point1.x + ',' + point1.y +
-      'L' + point1.x + ' ' + (point2.y + options.radius) +
-      'a' + options.radius + ',' + options.radius + ' 1 0 0 ' + -options.radius + ',' + -options.radius +
-      'L' + point2.x + ' ' + point2.y
-    )
-  }
-
-  branchLeftRoundedRect = (point1, point2, options) => {
-    // const radius = 40
-    return (
-      'M' + point1.x + ',' + point1.y +
-      'L' + point1.x + ' ' + ((point1.y - point2.y) / 2 + point2.y + options.radius) +
-      'a' + options.radius + ',' + options.radius + ' 0 0 1 ' + options.radius + ',' + -options.radius +
-      'L' + (oint1.x + options.distance - options.radius) + ' ' + ((point1.y - point2.y) / 2 + point2.y) +
-      'a' + options.radius + ',' + options.radius + ' 0 0 0 ' + options.radius + ',' + -options.radius +
-      'L' + (oint1.x + options.distance) + ' ' + (point2.y + options.radius) +
-      'a' + options.radius + ',' + options.radius + ' 0 0 0 ' + -options.radius + ',' + -options.radius +
-      'L' + point2.x + ' ' + point2.y
-    )
-  }
-
-  branchRightRoundedRect = (point1, point2, options) => {
-    // const radius = 40
-    return (
-      'M' + point1.x + ',' + point1.y +
-      'L' + point1.x + ' ' + ((point1.y - point2.y) / 2 + point2.y + options.radius) +
-      'a' + options.radius + ',' + options.radius + ' 0 0 1 ' + options.radius + ',' + -options.radius +
-      'L' + ((point2.x - point1.x) / 2 + point1.x - options.radius) + ' ' + ((point1.y - point2.y) / 2 + point2.y) +
-      'a' + options.radius + ',' + options.radius + ' 0 0 0 ' + options.radius + ',' + -options.radius +
-      'L' + ((point2.x - point1.x) / 2 + point1.x) + ' ' + (point2.y + options.radius) +
-      'a' + options.radius + ',' + options.radius + ' 0 0 1 ' + options.radius + ',' + -options.radius +
-      'L' + point2.x + ' ' + point2.y
-    )
-  }
-
-  getLine = (point1, point2, options) => {
-    if (options.type === 'BoundedLeft') return this.branchLeftRoundedRect(point1, point2, options);
-    if (options.type === 'BoundedRight') return this.branchRightRoundedRect(point1, point2, options)
-    if (options.type === 'Left') return this.leftLineRect(point1, point2, options)
-    if (options.type === 'Right') return this.rightRoundedRect(point1, point2, options)
-    return null
-  }
-
-  getPointAlLengthWithRotation = (path, length) => {
-    let p1 = path.getPointAtLength(length);
-    let p2 = path.getPointAtLength(length + 3);
-    let deg = Math.atan2(p1.y - p2.y, p1.x - p2.x) * (180 / Math.PI);
-    return {
-      x: p1.x,
-      y: p1.y,
-      angle: deg,
-    };
-  };
-
-  animate(index) {
-    this.animateValue1.addListener(dashOffset => {
-      this.refs.path1.setNativeProps({
-        strokeDashoffset: dashOffset.value,
-      });
+  createAllTree() {
+    const { data } = this.state;
+    data.map((item, index) => {
+      const { options, point1, point2 } = item;
+      item.ref = index;
+      const d = getLine(point1, point2, { ...options, ...{ radius: 40 } });
+      item.d = d;
+      const properties = path.svgPathProperties(d);
+      const tl = properties.getTotalLength();
+      item.tl = tl;
     });
-    this.animateValue2.addListener(dashOffset => {
-      this.refs.path2.setNativeProps({
-        strokeDashoffset: dashOffset.value,
-      });
+    this.setState({ data }, () => {
+      this.startAnimation(this.createAnimation());
     });
-    Animated.parallel([
-      Animated.timing(this.animateValue1, {
-        toValue: this.tl1,
-        duration: 800,
-        useNativeDriver: true,
-      }).start(() => {
-        this.props.changeData(index)
-      }),
-      Animated.timing(this.animateValue2, {
-        toValue: this.tl2,
-        duration: 800,
-        useNativeDriver: true,
-      }).start(() => {
-        this.props.changeData(index)
-      })
-    ]).start()
-
   }
 
-  changeData = index => {
-    this.animate(index)
-  }
-
-  renderImg() {
-    const { data } = this.props
+  createAnimation() {
+    const { data } = this.state;
     return data.map((item, index) => {
-      if (!item.isHidden) return <ImageAvatar
-        key={item.id}
-        data={item}
-        index={index}
-        source={Images.user}
-        style={{ backgroundColor: 'green' }}
-        changeData={this.changeData.bind(this)} />
-      return null
-    })
+      this.animatedValue[index].addListener(dashOffset => {
+        this.refs[index].setNativeProps({
+          strokeDashoffset: dashOffset.value,
+        });
+      });
+      return Animated.timing(this.animatedValue[index], {
+        toValue: item.tl,
+        duration: 300,
+        useNativeDriver: true,
+      });
+    });
+  }
+
+  startAnimation(animations) {
+    Animated.sequence(animations).start(() => { });
   }
 
   render() {
-    const { isAnimated } = this.props
-
+    const { data } = this.state;
     return (
-      <View style={this.props.style}>
-        <Svg style={{ alignItems: 'center', justifyContent: 'center', backgroundColor: 'pink' }} width={width} height={height}>
-          <Path
-            ref="path1"
-            d={this.d1}
-            strokeDasharray={[900, 900]}
-            strokeLinecap="round"
-            strokeDashoffset={899}
-            stroke="green" strokeWidth={6} fill="none" />
-          <Path
-            ref="path2"
-            d={this.d2}
-            strokeDasharray={[900, 900]}
-            strokeLinecap="round"
-            strokeDashoffset={899}
-            stroke="green" strokeWidth={6} fill="none" />
-          {isAnimated ? this.renderImg() : null}
-        </Svg>
-
-      </View>
-
+      <Svg
+        width={width}
+        height={height}
+        style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        {data.map((item, index) => {
+          return (
+            <Path
+              key={index}
+              ref={index}
+              d={item.d}
+              strokeDasharray={[1000, 1000]}
+              strokeLinecap="round"
+              strokeDashoffset={999}
+              stroke={item.options.color || 'green'}
+              strokeWidth={6}
+              fill="none"
+            />
+          );
+        })}
+      </Svg>
     );
   }
 }
